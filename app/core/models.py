7aef73c9613a54968a9d4fe883711b6d64639916
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-10-23 18:31:13 +0530

    Changes in user model

--- app/core/models.py
+++ app/core/models.py
@@ -11,8 +11,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create save and return a new user"""
-        user = self.model(email=email, **extra_fields) #
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields) #
         user.set_password(password) #set_password is a helper func from django to encrypt the password
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True #is_superuser is a field in PermissionsMixin
         user.save(using=self._db)
 
         return user
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -19,4 +19,31 @@ class ModelsTests(TestCase):
         )
 
         self.assertEqual(user.email, email) #check if email is same as the one we passed in
-        self.assertTrue(user.check_password(password)) #check if password is same as the one we passed in (check_password is a helper func from django as we are not storing password in plain text)) 
+        self.assertTrue(user.check_password(password)) #check if password is same as the one we passed in (check_password is a helper func from django as we are not storing password in plain text))
+        
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'test123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser) #is_superuser is a field in PermissionsMixin
+        self.assertTrue(user.is_staff)
